4aa0d49b9a884908214ba4cfa14b4fc85c0c414e
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-27 13:01:42 -0300

    Create auth.js

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (roles = []) => {
+  return (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) return res.sendStatus(401);
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      if (roles.length && !roles.includes(decoded.role)) {
+        return res.sendStatus(403);
+      }
+
+      req.user = decoded;
+      next();
+    } catch {
+      return res.sendStatus(401);
+    }
+  };
+};
