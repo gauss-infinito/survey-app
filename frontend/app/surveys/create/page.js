211044e188ddd8aafa366a7cf6bade92a9aa513d
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-28 20:43:25 -0300

    Update page.js

--- frontend/app/surveys/create/page.js
+++ frontend/app/surveys/create/page.js
@@ -2,8 +2,6 @@ import SurveyForm from "../../../components/SurveyForm";
 
 export default function CreateSurveyPage() {
   return (
-    <div style={{ padding: 20 }}>
-      <SurveyForm />
-    </div>
+    <SurveyForm />
   );
 }
