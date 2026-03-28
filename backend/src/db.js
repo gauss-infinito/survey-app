223497cd0d6830eb76b8a87f233de3d0ae022ba9
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-28 15:34:53 -0300

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "appdb",
+  database: process.env.DB_NAME || "survey",
 });
 
 module.exports = pool;
