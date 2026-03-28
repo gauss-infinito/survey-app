ff5a52db245113f57c850ce049752c58b5787f51
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-28 15:26:21 -0300

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || "postgres",
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "appdb",
 });
 
 module.exports = pool;
