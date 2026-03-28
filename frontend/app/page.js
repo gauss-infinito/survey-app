dda384537fe5930a1deedc03d9c9a619c31f1c2a
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-28 18:30:51 -0300

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,8 @@ export default function Home() {
     <main>
       <h1>Survey App</h1>
       <a href="/login">Login</a><br />
-      <a href="/register">Registre-se</a>
+      <a href="/register">Registre-se</a><br />
+      <a href="/surveys/create">Crie uma pesquisa</a>    
     </main>
   );
 }
