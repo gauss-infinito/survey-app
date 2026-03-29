6e0f3c31ef3b5e7afcbefcba53667ba19533ef6e
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-29 14:26:49 -0300

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <main>
       <div style={{ width: "277px", marginLeft: "28px" }}>
-        <h2>Survey App</h2>
+        <h1>Survey App</h1>
         <a href="/login">Login</a><br />
         <a href="/register">Registre-se</a><br />
+        <a href="/recover">Recupere seu código</a><br />  
         <a href="/surveys/create">Crie pesquisa</a>
       </div>
     </main>
