ee91342b70e7f82d42f3781edb5d02019ce64643
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-29 15:06:21 -0300

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,23 +1,9 @@
-export default function Home() {
-  return (
-    <main>
-      <div style={{ width: "277px", marginLeft: "28px" }}>
-        <h1>Survey App</h1>
-        <a href="/login">Login</a><br />
-        <a href="/register">Registre-se</a><br />
-        <a href="/recover">Recupere seu código</a><br />  
-        <a href="/surveys/create">Crie pesquisa</a>
-      </div>
-    </main>
-  );
-}
-
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div style={{ maxWidth: "600px", marginLeft: "40px", fontFamily: "Arial" }}>
+      <div style={{ maxWidth: "600px", marginLeft: "26px" }}>
         
         <h1>Survey App</h1>
 
