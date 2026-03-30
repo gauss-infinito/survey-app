3cc3be2147dac9de86a2b986ea1237cef41ef010
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-30 09:15:56 -0300

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,5 +1,9 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://survey-api-flaviacb-dev.apps.rm1.0a51.p1.openshiftapps.com";
+
 export async function createSurvey(data) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/surveys`, {
+  const res = await fetch(`${API_URL}/surveys`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
