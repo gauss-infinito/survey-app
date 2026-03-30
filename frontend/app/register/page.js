9579c4b59854fe531f994f9f75d0b1da96893ab2
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-03-30 09:24:24 -0300

    Update page.js

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -1,28 +1,30 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { API_URL } from "@/services/api";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [generatedCode, setGeneratedCode] = useState("");
 
   async function register() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, {
+    const res = await fetch(`${API_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, age, gender }),
     });
-  
+
     const data = await res.json();
-  
+
     localStorage.setItem("token", data.token);
     alert(`Guarde seu código: ${data.code}`);
     setGeneratedCode(data.code);
   }
-  
+ 
   return (
     <div style={{ width: "285px", marginLeft: "28px", fontFamily: "system-ui" }}>
       <h2>Registre-se</h2>
