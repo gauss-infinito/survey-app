0cbeb942dc7d7630ecce7eda95b492b452d2f8f5
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-04-03 17:45:34 -0300

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -38,6 +38,7 @@ export async function createSurvey(data) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(data),
   });
