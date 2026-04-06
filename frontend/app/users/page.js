3f7bd0ceb31d68397c1778407a2a299abee825e8
Flávia Bomfim <167620637+bomfimflavia@users.noreply.github.com>
2026-04-06 20:38:52 -0300

    Update page.js

--- frontend/app/users/page.js
+++ frontend/app/users/page.js
@@ -3,3 +3,11 @@ import UserManagement from "./pages/UserManagement";
 function App() {
   return <UserManagement />;
 }
+
+import SurveyForm from "../../../components/SurveyForm";
+
+export default function CreateSurveyPage() {
+  return (
+    <SurveyForm />
+  );
+}
